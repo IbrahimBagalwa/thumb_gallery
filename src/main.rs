86ab3af09d59b6d5bbee5f4c315f18ad46f8cb90
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2024-10-07 11:20:31 +0200

    feat: creating dabase and run migrations

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Ok;
+
+#[tokio::main]
+async fn main()-> anyhow::Result<()> {
+    dotenv::dotenv()?;
+    let db_connection_string = std::env::var("DATABASE_URL")?;
+    let connection_pool = sqlx::SqlitePool::connect(&db_connection_string).await?;
+
+    //Run migrations
+    sqlx::migrate!("./migrations")
+    .run(&connection_pool)
+    .await?;
+
+    Ok(())
 }
